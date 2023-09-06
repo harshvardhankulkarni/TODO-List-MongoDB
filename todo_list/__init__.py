d5eb0729b957796ff94f73282ac72bb8e6a5ffe3
Harsh <harshkulkarni78@gmail.com>
2023-09-06 17:46:42 +0530

    update .gitignore file and add mongodb atlas instead of local database

--- todo_list/__init__.py
+++ todo_list/__init__.py
@@ -3,7 +3,9 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
-app.config["MONGO_URI"] = "mongodb://localhost:27017/todoList"
+USERNAME = open('./todo_list/USERNAME.txt', 'r').read()
+PASSWORD = open('./todo_list/PASSWORD.txt', 'r').read()
+app.config["MONGO_URI"] = f'mongodb+srv://{USERNAME}:{PASSWORD}@firstmongoproject.aovehzm.mongodb.net/todo_list'
 db = PyMongo(app).db
 
 # Collections
