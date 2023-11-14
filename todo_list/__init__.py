904896b0b63de7ed4b8d391f1db60e59bfd6a29d
Harsh <harshkulkarni78@gmail.com>
2023-11-14 12:00:52 +0530

    add Session

--- todo_list/__init__.py
+++ todo_list/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_session import Session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
+
+app.secret_key = "Something Unique"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 USERNAME = open('./todo_list/USERNAME.txt', 'r').read()
 PASSWORD = open('./todo_list/PASSWORD.txt', 'r').read()
 app.config["MONGO_URI"] = f'mongodb+srv://{USERNAME}:{PASSWORD}@firstmongoproject.aovehzm.mongodb.net/todo_list'
